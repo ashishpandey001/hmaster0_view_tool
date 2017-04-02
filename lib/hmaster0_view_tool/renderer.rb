38fa36a34d1e3fd039692554569180d42108676e
Ashish Pandey <hmaster0@gmail.com>
2017-04-02 17:54:23 +0530

    Updated name of the copyright user to be in bold.

--- lib/hmaster0_view_tool/renderer.rb
+++ lib/hmaster0_view_tool/renderer.rb
@@ -1,7 +1,7 @@
 module Hmaster0ViewTool
   class Renderer
     def self.copyright(name, msg)
-      ActionController::Base.helpers.safe_join(["&copy; #{Time.zone.now.year} | #{name} #{msg}".html_safe])
+      ActionController::Base.helpers.safe_join(["&copy; #{Time.zone.now.year} | <b>#{name}</b> | #{msg}".html_safe])
     end
   end
 end
--- lib/hmaster0_view_tool/version.rb
+++ lib/hmaster0_view_tool/version.rb
@@ -1,3 +1,3 @@
 module Hmaster0ViewTool
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
